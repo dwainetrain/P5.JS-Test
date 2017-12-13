838470a05159660d8d409c7a7f79374966197542
dwainetrain <dwaine.best@gmail.com>
2017-12-13 13:01:50 -0800

    Committing some files to see if the locations work

---
+++ sketches/00_loop.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(700,400)
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+
+  if (mouseIsPressed) {
+    fill(0);
+  }else{
+    fill(255);
+  }
+
+rect(mouseX, mouseY, 50,50);
+
+}
